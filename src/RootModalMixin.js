5d576eaadd578b0d3204045cbf105363bca3d2ae
Nikita Nafranets <eddimensi@gmail.com>
2018-03-04 18:29:12 +0300

    add jsdoc comments in RootModalMixin

--- src/RootModalMixin.js
+++ src/RootModalMixin.js
@@ -1,20 +1,46 @@
+
+/**
+ * @typedef {Object.<string,any>} modal
+ * @prop {string} typeModal - key of modal
+ * @prop {number} [modalId] - id of modal
+ * @prop {function} resolve - resolve func from promise
+ * @prop {function} reject - reject func from promise
+ */
 export default {
   name: 'vue-root-modal',
   data () {
     return {
+      /**
+       * link on all opened modals
+       */
       modals: this.$modals.$openedModals,
     }
   },
+  /**
+   * Add all components from VueRootModals on beforeCreate
+   * @returns {void}
+   */
   beforeCreate () {
     this.$options.components = this.$modals.$components
   },
   methods: {
+    /**
+     * Return style object for modal
+     *
+     * @param {modal} modal
+     */
     zIndex (modal) {
       return {
         'z-index': modal.modalID,
       }
     },
 
+    /**
+     * Close modal by modal.modalID
+     *
+     * @param {modal} modal
+     * @returns {void}
+     */
     closeModal (modal) {
       if (modal.disableClose) {
         return
